3e450a0b0a28608eaee0fe320996c3b05e09ef59
Amol Umbarkar <amolumbarkar@Amols-Laptop.local>
2024-12-07 16:25:52 +0530

    added fast api template v2

--- inference/starter-kit-fastapi/app.py
+++ inference/starter-kit-fastapi/app.py
@@ -8,6 +8,10 @@ from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel, Field, root_validator, validator
 from typing import Any, List, Optional
 
+from transformers import AutoModelForCausalLM, AutoTokenizer
+import torch
+
+
 logging.basicConfig(level=logging.INFO)
 log = logging.getLogger(__name__)
 
@@ -39,56 +43,53 @@ if ENABLE_CORS:
         allow_headers=["*"],
     )
 
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+# TODO: edit model name here as per your requirement. 
+
+model_name_or_path = 'meta-llama/Meta-Llama-3-8B'
+
+# If you are using TIR model repository, and the model is fully merged (not peft or lora) then 
+# set the model name to below instead by uncommenting the line. This is to make sure the loaded model 
+# is from TIR model repository which downloads models to /mnt/models location before starting container.
+# model_name_or_path = '/mnt/models' 
+# 
+tokenizer = AutoTokenizer.from_pretrained(model_name_or_path)
+
+base_model = AutoModelForCausalLM.from_pretrained(model_name_or_path, torch_dtype=torch.float16, device_map='auto')
+model = base_model 
+
+# if you have peft model then uncomment the following. This code is expecting the fine-tuned weights (lora weights) in 
+# location /mnt/models. If you have used TIR Model repository then the files will be automatically downloaded 
+# to /mnt/models by TIR. But if you are uploading the weights to docker image then make sure the location is correctly set.
+
+# from peft import PeftModel, PeftConfig
+# peft_model = PeftModel.from_pretrained(base_model, "/mnt/models")
+# model  = peft_model 
+
+model = model.to("cuda")
+model.eval()
+
+
 @app.get("/health")
 def health():
     return {"status": "ok"}
 
-# This  request format is for reference only. You can change it or define as per your needs
-class RequestBody(BaseModel):
-    messages: List[dict] = Field(
-        default=None, description="The list of messages in the current conversation."
-    )
-    context: Optional[dict] = Field(
-        default=None,
-        description="Additional context data to be added to the conversation.",
-    )
-    stream: Optional[bool] = Field(
-        default=False,
-        description="If set, partial message deltas will be sent, like in ChatGPT. "
-        "Tokens will be sent as data-only server-sent events as they become "
-        "available, with the stream terminated by a data: [DONE] message.",
-    )
-    state: Optional[dict] = Field(
-        default=None,
-        description="A state object that should be used to continue the interaction.",
-    )
 
-# This response object is for reference only. You may change it or define your own response format. 
-class ResponseBody(BaseModel):
-    messages: List[dict] = Field(
-        default=None, description="The new messages in the conversation"
-    )
-    llm_output: Optional[dict] = Field(
-        default=None,
-        description="Contains any additional output coming from the LLM.",
-    )
-    output_data: Optional[dict] = Field(
-        default=None,
-        description="The output data, i.e. a dict with the values corresponding to the `output_vars`.",
-    )
-    state: Optional[dict] = Field(
-        default=None,
-        description="A state object that should be used to continue the interaction in the future.",
-    )
+class MessageRequest(BaseModel):
+    messages: str
+
+@app.post("/v1/chat/completions")
+async def chat_completion(message: MessageRequest):
     
-@app.post(
-    "/v1/chat/completions",
-    response_model=ResponseBody
-)
-async def chat_completion(body: RequestBody, request: Request):
-    # TODO: Write code to handle requests 
+    # here we are just using the first message for testing. In production, make sure all messages are used.
+    inputs = tokenizer(message.messages[0], return_tensors="pt")
 
-    return {"messages": "[\"system\": \"here is response\"]"}
+    with torch.no_grad():
+        outputs = model.generate(input_ids=inputs["input_ids"].to("cuda"), max_new_tokens=100)
+        print(tokenizer.batch_decode(outputs.detach().cpu().numpy(), skip_special_tokens=True)[0])
+
+    return {"messages": outputs}
 
 
 if __name__ == "__main__":
